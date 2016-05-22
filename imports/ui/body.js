427e8a6f3919f89394829280fd0658b4588a402e
Frederico Maia Arantes <frederico.maia@tecsinapse.com.br>
2016-05-22 17:30:04 -0300

    Adicionando package do Electron para rodar em desktop

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,17 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { Tasks } from '../api/tasks.js';
-import './body.html';
-import './task.js';
+import {Meteor} from "meteor/meteor";
+import {Template} from "meteor/templating";
+import {ReactiveDict} from "meteor/reactive-dict";
+import {Tasks} from "../api/tasks.js";
+import "./body.html";
+import "./task.js";
 
 Template.body.onCreated(function bodyOnCreatead() {
   this.state = new ReactiveDict();
   Meteor.subscribe("tasks");
+
+  // if (!Electron.isDesktop()){ Não roda no desktop
+  //   alert("Já pensou em baixar nossa app para o seu pc?");
+  // }
 });
 
 Template.body.helpers({
